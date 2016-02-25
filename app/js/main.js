8fbef502b5076aa0a92ced9e88ad6e9b63e5f437
jjelfs <jjelfs@travelrepublic.co.uk>
2016-02-25 22:25:38 +0000

    first commit

---
+++ app/js/main.js
@@ -0,0 +1,110 @@
+// IMPORT ASSETS
+import '../css/App.scss'
+import '../img/feed.png'
+import '../img/split.png'
+import virusImage from '../img/virus.png'
+import '../audio/spawn.mp3'
+import '../audio/split.mp3'
+import { THREE } from 'three'
+import orbitFn from 'three-orbit-controls'
+const OrbitControls = orbitFn(THREE)
+
+import io from 'socket.io-client'
+
+function debug(msg) {
+    console.log(msg)
+}
+
+let camera, controls, scene, renderer
+
+init()
+animate()
+
+function init() {
+    scene = new THREE.Scene()
+    scene.fog = new THREE.FogExp2(0xcccccc, 0.002)
+    renderer = new THREE.WebGLRenderer()
+    renderer.setClearColor(scene.fog.color)
+    renderer.setPixelRatio(window.devicePixelRatio)
+    renderer.setSize(window.innerWidth, window.innerHeight)
+
+    let container = document.getElementById('container')
+    container.appendChild(renderer.domElement)
+
+    camera = new THREE.PerspectiveCamera(60, window.innerWidth / window.innerHeight, 1, 1000)
+    camera.position.z = 500
+
+    controls = new OrbitControls(camera, renderer.domElement)
+    controls.enableDamping = true
+    controls.dampingFactor = 0.25
+    controls.enableZoom = true
+
+    const geometry = new THREE.SphereGeometry(20, 20, 20)
+    const material = new THREE.MeshPhongMaterial({color: 0x00ffff, shading: THREE.FlatShading})
+
+    for (let i = 0; i < 1000; i++) {
+        const mesh = new THREE.Mesh(geometry, material)
+        mesh.position.x = ( Math.random() - 0.5 ) * 1000
+        mesh.position.y = ( Math.random() - 0.5 ) * 1000
+        mesh.position.z = ( Math.random() - 0.5 ) * 1000
+        mesh.updateMatrix()
+        mesh.matrixAutoUpdate = false
+        scene.add(mesh)
+    }
+
+    let light = new THREE.DirectionalLight(0xffffff)
+    light.position.set(1, 1, 1)
+    scene.add(light)
+
+    light = new THREE.DirectionalLight(0x002288)
+    light.position.set(-1, -1, -1)
+    scene.add(light)
+
+    light = new THREE.AmbientLight(0x222222)
+    scene.add(light)
+
+    window.addEventListener('resize', onWindowResize, false)
+}
+
+function onWindowResize() {
+    camera.aspect = window.innerWidth / window.innerHeight
+    camera.updateProjectionMatrix()
+    renderer.setSize(window.innerWidth, window.innerHeight)
+}
+
+function render() {
+    //camera.position.z -= 1
+    renderer.render(scene, camera)
+}
+
+function animate() {
+    requestAnimationFrame(animate)
+    controls.update() // required if controls.enableDamping = true, or if controls.autoRotate = true
+    render()
+}
+
+const socket = io()
+
+function setupSocket() {
+    socket.on('pong', () => {
+        debug('pong')
+    })
+
+    // Handle error.
+    socket.on('connect_failed', () => {
+        socket.close()
+        disconnected = true
+    })
+
+    socket.on('disconnect', () => {
+        socket.close()
+        disconnected = true
+    })
+
+    // Handle connection.
+    socket.on('welcome', () => {
+        debug('welcome')
+    })
+}
+setupSocket(socket)
+
