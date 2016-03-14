8ea7a13d85339c9b4e077ebd3e7a3312247be489
julianjelfs <julian.jelfs@gmail.com>
2016-03-14 08:00:38 +0000

    note to self

--- src/agario3d/game_loop.clj
+++ src/agario3d/game_loop.clj
@@ -18,4 +18,6 @@
           (when-let [[value port] (alts! [ws-channel tick-ch])]
             (condp = port
               ws-channel (recur (player-command (:message value))) ;;this happens when we get a message from the client
+              ;;this is wrong we don't want to update game every tick because this runs for
+              ;;every connected player. We need a separate loop that runs once for all players
               tick-ch (recur (update-game))))))))  ;;this happens every tick of the game loop
